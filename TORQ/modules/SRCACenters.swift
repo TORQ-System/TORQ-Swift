96e39725da4223f4d835eba606ab7888b3a3b05d
= <Nouraaalsulayfih@gmail.com>
2021-10-02 18:41:28 +0300

    Added srca centers
    added the srca centers dictionary along with the most important information that we need to know about

---
+++ TORQ/modules/SRCACenters.swift
@@ -0,0 +1,185 @@
+import Foundation
+
+
+class SCRACenters {
+    
+    let centers = [
+    
+       [
+        "":"",
+        "":24.0000,
+        "":24.000
+       ],
+    ]
+    
+    let  SRCACenters: [[String:Any]] = [
+      [
+        // 1-maseef
+        "name": "maseef",
+        "latitude": 24.766719982385496,
+        "longitude": 46.67534595473277
+      ],
+      [
+        // 2-malqa
+        "name": "malqa",
+        "latitude": 24.813340812816,
+        "longitude": 46.608601997063275,
+      ],
+      [
+        // 3-khaldiya
+        "name": "khaldiya",
+        "latitude": 24.619958154092846,
+        "longitude": 46.75525738171758,
+      ],
+      [
+        // 4-king fahad
+        "name": "kingFahad",
+        "latitude": 24.739310634888415,
+        "longitude": 46.674034268226166,
+      ],
+      [
+        // 5-uraubah
+        "name": "uraubah",
+        "latitude": 24.70462364665244,
+        "longitude": 46.64807129759366,
+      ],
+      [
+        // 6-nafel
+        "name": "nafel",
+        "latitude": 24.78216370748611,
+        "longitude": 46.680739075994765,
+      ],
+      [
+        // 7-sulaimaniyah
+        "name": "sulaimaniyah",
+        "latitude": 24.710575659754106,
+        "longitude": 46.69030329759365,
+      ],
+      [
+        // 8-rawdah
+        "name": "rawdah",
+        "latitude": 24.739990461910555,
+        "longitude": 46.75482089759365,
+      ],
+      [
+        // 9-king faisal
+        "name": "kingFaisal",
+        "latitude": 24.758385968298718,
+        "longitude": 46.77954013417631,
+      ],
+      [
+        // 10-Nahdah
+        "name": "Nahdah",
+        "latitude": 24.764932864314947,
+        "longitude": 46.824687073212694,
+      ],
+      [
+        // 11-Ishbeliyah
+        "name": "Ishbeliyah",
+        "latitude": 24.79760402644183,
+        "longitude": 46.7870465262868,
+      ],
+      [
+        // 12-Diriyah<3<3
+        "name": "Diriyah",
+        "latitude": 24.751311059443832,
+        "longitude": 46.56843443622849,
+      ],
+      [
+        // 13-Irqah
+        "name": "Irqah",
+        "latitude": 24.685478009425,
+        "longitude": 46.588807536228494,
+      ],
+      [
+        // 14-rabwah
+        "name": "rabwah",
+        "latitude": 24.691632342324862,
+        "longitude": 46.7644165111836,
+      ],
+      [
+        // 15-dhbab
+        "name": "dhbab",
+        "latitude": 24.663302422873645,
+        "longitude": 46.71039090521693,
+      ],
+      [
+        // 16-laban
+        "name": "laban",
+        "latitude": 24.65519378984205,
+        "longitude": 46.587670368287135,
+      ],
+      [
+        // 17-naseem
+        "name": "naseem",
+        "latitude": 24.751125038650233,
+        "longitude": 46.82915955833446,
+      ],
+      [
+        // 18-rimal
+        "name": "rimal",
+        "latitude": 24.835299420469504,
+        "longitude": 46.80158764475565,
+      ],
+      [
+        // 19-khaleej
+        "name": "khaleej",
+        "latitude": 24.786104788767688,
+        "longitude": 46.80613437245616,
+      ],
+      [
+        // 20-twaiq
+        "name": "twaiq",
+        "latitude": 24.585265317480754,
+        "longitude": 46.547105141719804,
+      ],
+      [
+        // 21-munsiyah
+        "name": "munsiyah",
+        "latitude": 24.824786989529542,
+        "longitude": 46.81655385515806,
+      ],
+      [
+        // 22-namar
+        "name": "namar",
+        "latitude": 24.55211787741151,
+        "longitude": 46.63131015472793,
+      ],
+      [
+        // 23-nadwah
+        "name": "nadwah",
+        "latitude": 24.79054888623931,
+        "longitude": 46.876853868227386,
+      ],
+      [
+        // 24-dar bida
+        "name": "darBida",
+        "latitude": 24.567103148393645,
+        "longitude": 46.7685939682224,
+      ],
+      [
+        // 25-bayan
+        "name": "bayan",
+        "latitude": 24.88285664307162,
+        "longitude": 46.85941051240605,
+      ],
+      [
+        // 26-narjis
+        "name": "narjis",
+        "latitude": 24.839968700490484,
+        "longitude": 46.67215439706382,
+      ],
+      [
+        // 27-salalm
+        "name": "salalm",
+        "latitude": 24.71603727390603,
+        "longitude": 46.811516483566905,
+      ],
+      [
+        // 28-qaser Alhukum
+        "name": "qaserAlhukum",
+        "latitude": 24.62625068416492,
+        "longitude": 46.70996480685846,
+      ]
+    ];
+}
---
+++ TORQ/viewControllers/paramedicHomeViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import CoreLocation
+
+class paramedicHomeViewController: UIViewController {
+
+    //MARK: - @IBOutlets
+    @IBOutlet weak var closest: UILabel!
+    
+    //MARK: - Variables
+    
+    
+    //MARK: - Overriten Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Functions
+    func neraest(){
+        
+        
+    }
+    
+    //MARK: - @IBActions
+    
+    
+
+}
+
