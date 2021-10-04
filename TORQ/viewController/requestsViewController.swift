a3126806cb33c01cebe0d3218ef8ae39e9210cc4
= <51061154+shahad0210@users.noreply.github.com>
2021-10-04 03:51:07 +0300

    integration with nearest branch
    this branch is viewing the request if the logged in center in the nearest to the accident location.

--- TORQ/viewController/requestsViewController.swift
+++ TORQ/viewController/requestsViewController.swift
@@ -10,7 +10,9 @@ class requestsViewController: UIViewController {
     //MARK: - Variables
     var ref = Database.database().reference()
     var requests: [Request] = []
-    
+    var loggedInCenterEmail: String!
+    var loggedInCenter: [String: Any]?
+    var myRequests: [Request] = []
     //MARK: - Overriden function
     
     override func viewDidLoad() {
@@ -27,8 +29,29 @@ class requestsViewController: UIViewController {
         ref.child("Request").observe(.childAdded) { snapshot in
             let object = snapshot.value as! [String: Any]
             let request = Request(user_id: object["user_id"] as! Int, sensor_id: object["sensor_id"] as! Int, request_id: object["request_id"] as! Int, date: object["date"] as! String, time: object["time"] as! String, longitude: object["longitude"] as! Double, latitude: object["latitude"] as! Double, vib: object["vib"] as! Int, rotation: object["rotation"] as! Int, status: object["status"] as! Int)
-            self.requests.append(request)
+//            self.requests.append(request)
+            self.nearest(longitude: request.getLongitude(), latitude: request.getLatitude(), request: request)
+//            self.requestsColletionView.reloadData()
+
+        }
+    }
+    
+    func setupTheHardcoded(){
+        loggedInCenterEmail = "almalqa@srca.org.sa"
+    }
+    
+    func configureCenter(){
+        let domainRange = loggedInCenterEmail.range(of: "@")!
+        let centerName = loggedInCenterEmail[..<domainRange.lowerBound]
+        loggedInCenter = SRCACenters.getSRCAInfo(name: String(centerName))
+    }
+    
+    func nearest(longitude: Double, latitude:Double, request: Request){
+        let nearest = SRCACenters.getNearest(longitude: longitude, latitude: latitude)
+        if nearest["name"] as! String == "almalqa" {
+            myRequests.append(request)
             self.requestsColletionView.reloadData()
+
         }
     }
 
@@ -43,8 +66,8 @@ extension requestsViewController: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "viewLocation") as! viewLocationViewController
-        vc.latitude = requests[indexPath.row].getLatitude()
-        vc.longitude = requests[indexPath.row].getLongitude()
+        vc.latitude = myRequests[indexPath.row].getLatitude()
+        vc.longitude = myRequests[indexPath.row].getLongitude()
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true, completion: nil)
     }
@@ -53,7 +76,7 @@ extension requestsViewController: UICollectionViewDelegate{
 
 extension requestsViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return requests.count
+        return myRequests.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -62,9 +85,9 @@ extension requestsViewController: UICollectionViewDataSource{
         cell.layer.borderWidth = 2
         cell.layer.masksToBounds = true;
         cell.layer.cornerRadius = 20
-        cell.dateTime.text = "\(requests[indexPath.row].getDate())   \(requests[indexPath.row].getTime())"
+        cell.dateTime.text = "\(myRequests[indexPath.row].getDate())   \(myRequests[indexPath.row].getTime())"
         var state = "Processed"
-        if requests[indexPath.row].getStatus() == 0 {
+        if myRequests[indexPath.row].getStatus() == 0 {
             state = "Active"
         }
         cell.status.text = "status: \(state)"
@@ -81,4 +104,3 @@ extension requestsViewController: UICollectionViewDelegateFlowLayout{
     }
     
 }
-
