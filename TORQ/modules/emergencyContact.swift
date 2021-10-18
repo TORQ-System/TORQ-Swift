ed7926f012579320b720e64888c1ad5c47e4008d
NouraMajed <nouramajedalsalem@gmail.com>
2021-10-18 13:09:49 +0300

    added emergencyContact module

---
+++ TORQ/modules/emergencyContact.swift
@@ -0,0 +1,57 @@
+//
+//  emergencyContact.swift
+//  TORQ
+//
+//  Created by Norua Alsalem on 18/10/2021.
+//
+
+import Foundation
+import Foundation
+
+struct emergencyContact {
+    var name: String
+    var phone_number: String
+    var senderID: String
+    var recieverID: String
+    var sent: String
+    var contactID: Int
+    var msg: String
+
+    init(name: String, phone_number: String, senderID: String, recieverID: String, sent: String, contactID: Int, msg: String) {
+        self.name = name
+        self.phone_number = phone_number
+        self.senderID = senderID
+        self.recieverID = recieverID
+        self.contactID = contactID
+        self.msg = msg
+        self.sent = sent
+    }
+    
+    func getName()->String{
+        return name
+    }
+    
+    func getPhoneNumber()->String{
+        return phone_number
+    }
+
+    func getSenderID()->String{
+        return senderID
+    }
+    
+    func getReciverID()->String{
+        return recieverID
+    }
+    
+    func getContactID()->Int{
+        return contactID
+    }
+    
+    func getMsg()->String{
+        return msg
+    }
+    
+    func getSent()->String{
+        return sent
+    }
+}
