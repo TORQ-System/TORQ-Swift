8115b47aae7f3968910da4f4f445ed9f81508a94
NouraMajed <nouramajedalsalem@gmail.com>
2021-10-18 13:07:36 +0300

    update dataBase info

--- TORQ/modules/emergencyContact.swift
+++ TORQ/modules/emergencyContact.swift
@@ -9,11 +9,20 @@ import Foundation
 struct emergencyContact {
     var name: String
     var phone_number: String
+    var senderID: String
+    var recieverID: String
+    var sent: String
+    var contactID: Int
+    var msg: String
 
-    init(name: String, phone_number: String ) {
+    init(name: String, phone_number: String, senderID: String, recieverID: String, sent: String, contactID: Int, msg: String) {
         self.name = name
         self.phone_number = phone_number
-
+        self.senderID = senderID
+        self.recieverID = recieverID
+        self.contactID = contactID
+        self.msg = msg
+        self.sent = sent
     }
     
     func getName()->String{
@@ -24,4 +33,23 @@ struct emergencyContact {
         return phone_number
     }
 
+    func getSenderID()->String{
+        return senderID
+    }
+    
+    func getReciverID()->String{
+        return recieverID
+    }
+    
+    func getContactID()->Int{
+        return contactID
+    }
+    
+    func getMsg()->String{
+        return msg
+    }
+    
+    func getSent()->String{
+        return sent
+    }
 }
