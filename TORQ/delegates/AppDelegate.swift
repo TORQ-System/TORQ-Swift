7bfb636983550992b20cc4085bfd181dccbaae35
Dalal Bin Humaid <dalalbinhumaid@gmail.com>
2021-10-18 04:07:12 +0300

    added basic notifications

--- TORQ/delegates/AppDelegate.swift
+++ TORQ/delegates/AppDelegate.swift
@@ -10,29 +10,62 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        userNotificationConfig()
         FirebaseApp.configure()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
+extension AppDelegate{
+    private func userNotificationConfig(){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge,.alert,.sound,.carPlay]) {granted, error in
+            if error == nil {
+                print("premission granted: \(granted)")
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
+    }
+}
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier{
+        case "okay_action":
+            print("user is okay")
+            return
+        case "request_action":
+            print("user wants help")
+            return
+        default:
+            print("No reply")
+        }
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+}
--- TORQ/delegates/SceneDelegate.swift
+++ TORQ/delegates/SceneDelegate.swift
@@ -8,45 +8,85 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        
+        triggerLocalNotification(title: "Are you okay?", body: "We detcted an impact on your veichle, reply within 10s or we will send an ambulance request.")
     }
-
-
+    
 }
 
+extension SceneDelegate{
+    
+    func triggerLocalNotification(title: String, body: String){
+        
+        //Assign contents
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        content.categoryIdentifier = "okayCategory"
+        
+        //Create actions
+        let okayAction = UNNotificationAction(identifier: "okay_action", title: "I'm okay", options: UNNotificationActionOptions.init(rawValue: 0))
+        
+        let requestAction = UNNotificationAction(identifier: "request_action", title: "No, send request", options: UNNotificationActionOptions.init(rawValue: 0))
+        
+        let actionCategory = UNNotificationCategory(identifier: "okayCategory", actions: [okayAction, requestAction], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", options: .customDismissAction) //it will get dismissed
+        
+        
+        UNUserNotificationCenter.current().setNotificationCategories([actionCategory])
+        
+        //Trigger (time-based) - should be when a child is added from the database
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //Notification request
+        
+        let uuid = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+        
+        // reigester to the notification center
+        UNUserNotificationCenter.current().add(request) { error in
+            if error == nil{
+                NSLog("no error found")
+            }
+            else{
+                print("error found: ", error!.localizedDescription)
+            }
+        }
+    }
+}
