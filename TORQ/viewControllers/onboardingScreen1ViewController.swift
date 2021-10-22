d009e38449a6c6a5c19bbf5999d0b213e2aad9ca
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-10-22 21:04:12 +0300

    added buttons animations

--- TORQ/viewControllers/onboardingScreen1ViewController.swift
+++ TORQ/viewControllers/onboardingScreen1ViewController.swift
@@ -4,11 +4,38 @@ class onboardingScreen1ViewController: UIViewController {
 
     //MARK: - @IBOutlets
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var shapeContainer: UIView!
     
     //MARK: - Overriden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        nextButton.layer.cornerRadius = 50
+        nextButton.clipsToBounds = true
+        let bPath = UIBezierPath(arcCenter: CGPoint(x: nextButton.frame.width/2, y: nextButton.frame.height/2), radius: nextButton.frame.height/2, startAngle: 0, endAngle: (.pi * 2), clockwise: true)
+        
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = bPath.cgPath
+        trackLayer.lineWidth = 30
+        trackLayer.strokeColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        trackLayer.fillColor = UIColor.clear.cgColor
+        nextButton.layer.addSublayer(trackLayer)
+        let shape = CAShapeLayer()
+        shape.path = bPath.cgPath
+        shape.lineWidth = 30
+        shape.strokeColor = UIColor(red: 0.83921569, green: 0.33333333, blue: 0.42352941, alpha: 1).cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        nextButton.layer.addSublayer(shape)
+        
+            let anim = CABasicAnimation(keyPath: "strokeEnd")
+            anim.fromValue = 0
+            anim.toValue = 0.33333334
+            anim.duration = 1
+            anim.isRemovedOnCompletion = false
+            anim.fillMode = .forwards
+            shape.add(anim, forKey: "strokeEnd")
+        
     }
     
     //MARK: - Functions
@@ -32,3 +59,10 @@ class onboardingScreen1ViewController: UIViewController {
     }
     
 }
+
+
+
+
+//        nextButton.layer.cornerRadius = 45
+//        nextButton.clipsToBounds = true
+//        nextButton.backgroundColor = UIColor(red: 246, green: 235, blue: 235, alpha: 1)
--- TORQ/viewControllers/onboardingScreen3ViewController.swift
+++ TORQ/viewControllers/onboardingScreen3ViewController.swift
@@ -2,11 +2,43 @@ import UIKit
 
 class onboardingScreen3ViewController: UIViewController {
 
+    
+    //MARK: - @IBOutlets
+    @IBOutlet weak var shapeContainer: UIView!
+    
+    
+    //MARK: - Overriden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        shapeContainer.layer.cornerRadius = 50
+        shapeContainer.clipsToBounds = true
+        let bPath = UIBezierPath(arcCenter: CGPoint(x: shapeContainer.frame.width/2, y: shapeContainer.frame.height/2), radius: shapeContainer.frame.height/2, startAngle: 0, endAngle: (.pi * 2), clockwise: true)
+        
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = bPath.cgPath
+        trackLayer.lineWidth = 30
+        trackLayer.strokeColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
+        trackLayer.fillColor = UIColor.clear.cgColor
+        shapeContainer.layer.addSublayer(trackLayer)
+        let shape = CAShapeLayer()
+        shape.path = bPath.cgPath
+        shape.lineWidth = 30
+        shape.strokeColor = UIColor(red: 0.83921569, green: 0.33333333, blue: 0.42352941, alpha: 1).cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        shapeContainer.layer.addSublayer(shape)
+        
+            let anim = CABasicAnimation(keyPath: "strokeEnd")
+            anim.fromValue = 0
+            anim.toValue = 1
+            anim.duration = 1
+            anim.isRemovedOnCompletion = false
+            anim.fillMode = .forwards
+            shape.add(anim, forKey: "strokeEnd")
 
-        // Do any additional setup after loading the view.
     }
+    
+    //MARK: - @IBActions
     @IBAction func geToLoginButton(_ sender: Any) {
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "loginViewController") as! loginViewController
