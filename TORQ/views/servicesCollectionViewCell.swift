c012aaccefd4ebe5c0beb20fd7905e0ab3feff6c
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-10-25 05:54:40 +0300

    added service view collection cell

---
+++ TORQ/views/servicesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  servicesCollectionViewCell.swift
+//  TORQ
+//
+//  Created by Noura Alsulayfih on 25/10/2021.
+//
+
+import UIKit
+
+class servicesCollectionViewCell: UICollectionViewCell {
+    
+    //MARK:- @IBAoutlets
+    @IBOutlet weak var serviceImage: UIImageView!
+    @IBOutlet weak var serviceLabel: UILabel!
+    
+}
