a402d56a4736fea418cf42ca874a371160d2d756
Shahad <shahad.alshahrani02@gmail.com>
2021-10-27 21:47:00 +0300

    delete medical report

--- TORQ/viewControllers/viewMedicalReportViewController.swift
+++ TORQ/viewControllers/viewMedicalReportViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import SCLAlertView
 
 class viewMedicalReportViewController: UIViewController {
     
@@ -16,17 +18,24 @@ class viewMedicalReportViewController: UIViewController {
     @IBOutlet weak var addMedicalReport: UIButton!
     @IBOutlet weak var deleteMedicalReport: UIButton!
     
-    
     //MARK: - Variables
     var userID: String?
     
+    //MARK: - Constants
+    let redUIColor = UIColor( red: 200/255, green: 68/255, blue:86/255, alpha: 1.0 )
+    let alertIcon = UIImage(named: "errorIcon")
+    let apperance = SCLAlertView.SCLAppearance(
+        contentViewCornerRadius: 15,
+        buttonCornerRadius: 7,
+        hideWhenBackgroundViewIsTapped: true)
+    let ref = Database.database().reference()
+    
     
     //MARK: - Overriden Functions
-
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLayout()
-
+        
     }
     
     
@@ -57,8 +66,37 @@ class viewMedicalReportViewController: UIViewController {
     
     @IBAction func deleteMedicalReport(_ sender: Any) {
         // Shahad's code goes here
+        let alertView = SCLAlertView(appearance: self.apperance)
+        
+        alertView.addButton("Delete", backgroundColor: self.redUIColor){
+            self.ref.child("medical_info").queryOrdered(byChild:"user_id").observe(.childAdded, with: {(snapshot) in
+                if let dec = snapshot.value as? [String :Any]
+                {
+                    if (dec["user_id"] as! String == self.userID!){
+                        self.ref.child("medical_info").child(snapshot.key).removeValue()
+                        self.dismiss(animated: true, completion: nil)
+
+                    }
+                }
+            })
+        }
+        
+        alertView.showCustom("Are you sure?", subTitle: "We will delete your entire medical report", color: self.redUIColor, icon: self.alertIcon!, closeButtonTitle: "Cancel", circleIconImage: UIImage(named: "warning"), animationStyle: SCLAnimationStyle.topToBottom)
+        
+       
+        
+        //
+        //    ref.child("EmergencyContact").queryOrdered(byChild:"reciver").observe(.childAdded, with: {(snapshot) in
+        //        if let dec = snapshot.value as? [String :Any]
+        //        {
+        //            if (dec["reciver"] as! String == self.userID!){
+        //                self.ref.child("EmergencyContact").child(snapshot.key).removeValue()
+        //            }
+        //        }
+        //    })
+        
+        
     }
-    
 }
 
 //MARK: - Extensions
