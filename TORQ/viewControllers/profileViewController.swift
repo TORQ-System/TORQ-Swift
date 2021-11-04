1ff3ee50d986bed3af863b78f5287dfe1a05e20a
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-11-04 17:28:46 +0300

    added profile view

--- TORQ/viewControllers/profileViewController.swift
+++ TORQ/viewControllers/profileViewController.swift
@@ -10,10 +10,24 @@ import Firebase
 
 class profileViewController: UIViewController {
     
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var strokeView: UIView!
+    @IBOutlet weak var imageContainer: UIView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var joinedLabel: UILabel!
+    
+    
+    //MARK: - Variables
     var userID: String?
     let ref = Database.database().reference()
     var user: User?
-
+    var services = ["Terms and conditions","Settings","Edit Account","Sensor Information","Change passowrd","Logout"]
+    var servicesImages = ["list.bullet.rectangle","list.bullet.rectangle","list.bullet.rectangle","list.bullet.rectangle","list.bullet.rectangle","list.bullet.rectangle"]
+    
+    
+    //MARK: - Overriden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         let fetchQueue = DispatchQueue.init(label: "fetchQueue")
@@ -23,11 +37,11 @@ class profileViewController: UIViewController {
         
         
         
-        
 
     }
     
     
+    //MARK: - Functions
     private func retreieveUser(){
             ref.child("User").observe(.value) { snapshot in
                 for user in snapshot.children{
@@ -43,10 +57,31 @@ class profileViewController: UIViewController {
                         self.user = User(dateOfBirth: dateOfBirth,          email: email, fullName: fullName, gender:        gender, nationalID: nationalID, password:      password, phone: phone)
                         DispatchQueue.main.async {
                             //update UI here
-                            
+                            self.userName.text = fullName
+                            self.userEmail.text = email
+                            self.joinedLabel.text = "Joined 8 months ago"
                         }
                     }
                 }
             }
     }
+}
+
+//MARK: - Extension
+
+extension profileViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return services.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell") as! profileTableViewCell
+        cell.serviceLabel.text = services[indexPath.row]
+        
+        
+        
+        return cell
+    }
+    
+    
 }
---
+++ TORQ/views/profileTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  profileTableViewCell.swift
+//  TORQ
+//
+//  Created by Noura Alsulayfih on 04/11/2021.
+//
+
+import UIKit
+
+class profileTableViewCell: UITableViewCell {
+    
+    
+    //MARK:- @IBOutlts
+    @IBOutlet weak var serviceImage: UIImageView!
+    @IBOutlet weak var serviceLabel: UILabel!
+    @IBOutlet weak var seriviceButton: UIButton!
+    
+    
+    //MARK:- Overriden Functions
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
