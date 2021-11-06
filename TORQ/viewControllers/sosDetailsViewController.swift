e0a2604407852ba3ba462f95b57daeb23d6d86d0
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-11-06 07:30:24 +0300

    configuration of sosDetails View Controller

---
+++ TORQ/viewControllers/sosDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  sosDetailsViewController.swift
+//  TORQ
+//
+//  Created by Noura Alsulayfih on 06/11/2021.
+//
+
+import UIKit
+
+class sosDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
