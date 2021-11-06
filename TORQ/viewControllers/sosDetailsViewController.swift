f3da1798dc2fce32347aa92da5c5a99303b658cd
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-11-06 07:36:00 +0300

    set up outlets and actions

--- TORQ/viewControllers/sosDetailsViewController.swift
+++ TORQ/viewControllers/sosDetailsViewController.swift
@@ -8,22 +8,38 @@
 import UIKit
 
 class sosDetailsViewController: UIViewController {
+    
+    
+    //MARK: - @IBOutlets
+    @IBOutlet weak var customerService: UIButton!
+    @IBOutlet weak var cancel: UIButton!
+    @IBOutlet weak var liveLocation: UIButton!
+    
+    
+    
+    //MARK: - Variables
+    
+    
+    //MARK: - Overriden Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Functions
+    
+    
+    //MARK: - @IBActions
+    @IBAction func goChat(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func cancelSOSrequest(_ sender: Any) {
+        
+    }
+    
+    @IBAction func seeLiveLocation(_ sender: Any) {
+        
+    }
 }
