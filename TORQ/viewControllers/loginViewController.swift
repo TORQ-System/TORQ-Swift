d3555d60868d63153dce34bc21396de2431dbce9
Dalal Bin Humaid <dalalbinhumaid@gmail.com>
2021-11-15 14:34:18 +0300

    Consistent Forms

--- TORQ/viewControllers/loginViewController.swift
+++ TORQ/viewControllers/loginViewController.swift
@@ -8,33 +8,59 @@ class loginViewController: UIViewController {
     //MARK: - @IBOutlets
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var nextbutton: UIButton!
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var buttonView: UIView!
+    @IBOutlet weak var roundGradientView: UIView!
     
     //MARK: - Vraibales
+    var tap = UITapGestureRecognizer()
     var userID: String?
     var userEmail: String?
     
     //MARK: - Constants
     let redUIColor = UIColor( red: 200/255, green: 68/255, blue:86/255, alpha: 1.0 )
     let alertIcon = UIImage(named: "errorIcon")
-    let apperance = SCLAlertView.SCLAppearance(
-        contentViewCornerRadius: 15,
-        buttonCornerRadius: 7,
-        hideWhenBackgroundViewIsTapped: true)
+    let apperance = SCLAlertView.SCLAppearance(contentViewCornerRadius: 15, buttonCornerRadius: 7, hideWhenBackgroundViewIsTapped: true)
     
     //MARK: - Overriden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // setup default borders
         email.setBorder(color: "default", image: UIImage(named: "emailDefault")!)
         email.clearsOnBeginEditing = false
         password.setBorder(color: "default", image: UIImage(named: "lockDefault")!)
-        //set up the corner radius of login button
-        loginButton.layer.cornerRadius = 12
+        
+        configureTapGesture()
+        configureButtonView()
     }
     
     //MARK: - Functions
+    func configureTapGesture(){
+        tap = UITapGestureRecognizer(target: self, action: #selector(self.loginClicked(_:)))
+        tap.numberOfTapsRequired = 1
+        tap.numberOfTouchesRequired = 1
+        buttonView.addGestureRecognizer(tap)
+        buttonView.isUserInteractionEnabled = true
+    }
+    
+    func configureButtonView(){
+        roundGradientView.layer.cornerRadius = 20
+        roundGradientView.layer.shouldRasterize = true
+        roundGradientView.layer.rasterizationScale = UIScreen.main.scale
+        
+        let gradient: CAGradientLayer = CAGradientLayer()
+        
+        gradient.cornerRadius = 20
+        gradient.colors = [
+            UIColor(red: 0.887, green: 0.436, blue: 0.501, alpha: 1).cgColor,
+            UIColor(red: 0.75, green: 0.191, blue: 0.272, alpha: 1).cgColor
+        ]
+        
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.frame = roundGradientView.bounds
+        roundGradientView.layer.insertSublayer(gradient, at: 0)
+    }
+    
     func validateFields()->[String: String]{
         var errors = ["email":"", "password":""]
         
@@ -73,9 +99,38 @@ class loginViewController: UIViewController {
         present(vc, animated: true, completion: nil)
     }
     
+    @objc func loginClicked(_ sender: UITapGestureRecognizer) {
+        let errors = validateFields()
+        
+        guard errors["email"] == ""  else {
+            SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: errors["email"]!, color: self.redUIColor, icon: alertIcon!, closeButtonTitle: "Got it!", animationStyle: SCLAnimationStyle.topToBottom)
+            
+            return
+        }
+        
+        guard errors["password"] == ""  else {
+            SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: errors["password"]!, color: self.redUIColor, icon: alertIcon!, closeButtonTitle: "Got it!", animationStyle: SCLAnimationStyle.topToBottom)
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email.text!.trimWhiteSpace(), password: password.text!) { [self] authResult, error in
+            
+            guard error == nil else{
+                SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: "Incorrect email or password", color: self.redUIColor, icon: self.alertIcon!, closeButtonTitle: "Got it!", circleIconImage: UIImage(named: "warning"), animationStyle: SCLAnimationStyle.topToBottom)
+                return
+            }
+            if(self.email.text!.isParamedicUser){
+                self.goToParamedicHome()
+            }
+            else{
+                self.userID = authResult!.user.uid
+                self.goToUserHome()
+            }
+        }
+    }
+    
     //MARK: - @IBActions
     @IBAction func emailEditingChanged(_ sender: UITextField) {
-        // validate the email
         if sender.text == nil ||  sender.text == ""{
             sender.setBorder(color: "error", image: UIImage(named: "emailError")!)
         } else{
@@ -91,39 +146,6 @@ class loginViewController: UIViewController {
         }
     }
     
-    @IBAction func loginpressed(_ sender: Any) {
-        let errors = validateFields()
-        
-        // if there are any errors show the error view
-        guard errors["email"] == ""  else {
-            SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: errors["email"]!, color: self.redUIColor, icon: alertIcon!, closeButtonTitle: "Got it!", animationStyle: SCLAnimationStyle.topToBottom)
-            
-            return
-        }
-        
-        guard errors["password"] == ""  else {
-            SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: errors["password"]!, color: self.redUIColor, icon: alertIcon!, closeButtonTitle: "Got it!", animationStyle: SCLAnimationStyle.topToBottom)
-            return
-        }
-        
-        Auth.auth().signIn(withEmail: email.text!.trimWhiteSpace(), password: password.text!) { [self] authResult, error in
-            
-            guard error == nil else{
-                //self.showALert(message: "Please ensure all fields are correct")
-                SCLAlertView(appearance: self.apperance).showCustom("Invalid Credentials", subTitle: "Incorrect email or password", color: self.redUIColor, icon: self.alertIcon!, closeButtonTitle: "Got it!", circleIconImage: UIImage(named: "warning"), animationStyle: SCLAnimationStyle.topToBottom)
-                return
-            }
-            // role authentication
-            if(self.email.text!.isParamedicUser){
-                self.goToParamedicHome()
-            }
-            else{
-                self.userID = authResult!.user.uid
-                self.goToUserHome()
-            }
-        }
-    }
-    
     @IBAction func forgotPasswordPressed(_ sender: Any) {
         goToResetPassword()
     }
@@ -134,8 +156,6 @@ class loginViewController: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true, completion: nil)
     }
-    
-    
 }
 
 extension loginViewController: UITextFieldDelegate{
