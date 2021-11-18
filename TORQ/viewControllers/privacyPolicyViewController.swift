ce76f6b4d454be1997f9c7e74c1aaf160c848502
AleenWael <67905274+AleenWael@users.noreply.github.com>
2021-11-19 00:16:27 +0300

    Terms&Conditions and Privacy Policy Update
    fetching data from DB

--- TORQ/viewControllers/privacyPolicyViewController.swift
+++ TORQ/viewControllers/privacyPolicyViewController.swift
@@ -6,27 +6,40 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class privacyPolicyViewController: UIViewController {
-
+    
+    //MARK: - @IBOutlets
+    @IBOutlet weak var bodyLabel : UILabel!
+    
+    //MARK: - Variables
+    // DB reference
+    var ref = Database.database().reference()
+    var bodyText : String?
+    
+    //MARK: - Overriden Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchInfo()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    //MARK: - Functions
     @IBAction func back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    func fetchInfo(){
+        ref.child("PrivacyPolicy").observeSingleEvent(of: .value, with: { snapshot in
+            for terms in snapshot.children{
+                let obj = terms as! DataSnapshot
+                let body = obj.childSnapshot(forPath: "body").value as! String
+                self.bodyText = body
+                DispatchQueue.main.async{
+                    self.bodyLabel.text = self.bodyText
+                }
+            }
+        })
     }
 
 }
