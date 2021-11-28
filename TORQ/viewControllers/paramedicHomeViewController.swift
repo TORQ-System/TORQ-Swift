0436dac6a2a006132e6d88429a3049d7ff89cbe3
NouraMajed <nouramajedalsalem@gmail.com>
2021-11-28 18:38:34 +0300

    write paramedic location to database

--- TORQ/viewControllers/paramedicHomeViewController.swift
+++ TORQ/viewControllers/paramedicHomeViewController.swift
@@ -2,15 +2,14 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 import CoreLocation
+import SCLAlertView
 
 class paramedicHomeViewController: UIViewController {
-    
-    
-    //MARK: - @IBOutlets
     
     //MARK: - Variables
     var loggedinEmail: String!
     var location: [String: String] = ["lon":"","lat":""]
+
     
     //MARK: - Constants
     let locationManager = CLLocationManager()
@@ -19,48 +18,63 @@ class paramedicHomeViewController: UIViewController {
     //MARK: - Overriden funtions
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLocationManager()
     }
     
-    //MARK: - Functions
     func showALert(message:String){
-        //show alert based on the message that is being paased as parameter
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
-    func goToLoginScreen(){
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "loginViewController") as! loginViewController
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true, completion: nil)
+    func configureLocationManager(){
+        locationManager.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.showsBackgroundLocationIndicator = true
+        guard CLLocationManager.locationServicesEnabled() else {
+            self.showALert(message: "the location services isn't enabled")
+            return
+        }
+        locationManager.requestAlwaysAuthorization()
     }
+
+}
+
+
+//MARK: - CLLocationManagerDelegate Extensions
+
+extension paramedicHomeViewController: CLLocationManagerDelegate{
     
-    
-    //MARK: - @IBActions
-    @IBAction func viewRequestsPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "viewRequests") as! requestsViewController
-        vc.loggedInCenterEmail = self.loggedinEmail
-        print("home: \(String(describing: loggedinEmail))")
-        print("home: \(String(describing: vc.loggedInCenterEmail))")
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true, completion: nil)
-    }
-    
-    
-    @IBAction func logoutPressed(_ sender: Any) {
-        
-        do {
-            try Auth.auth().signOut()
-            let userDefault = UserDefaults.standard
-            userDefault.setValue(false, forKey: "isUserSignedIn")
-            goToLoginScreen()
-        } catch let error {
-            self.showALert(message: error.localizedDescription)
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+        case .authorizedAlways:
+            print("authorizedAlways")
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+            locationManager.startUpdatingLocation()
+        case .denied:
+            print("denied")
+             showALert(message: "Location access is needed to get your current location")
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+            showALert(message: "Location access is needed to get your current location")
+        default:
+            print("unknown")
         }
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let longitude = locations.last?.coordinate.longitude
+        let latitude = locations.last?.coordinate.latitude
+        location["lon"] = String(describing: longitude!)
+        location["lat"] = String(describing: latitude!)
+        ref.child("Paramedic").child("\(loggedinEmail!)/longitude").setValue((String(describing: longitude!)))
+        ref.child("Paramedic").child("\(loggedinEmail!)/latitude").setValue((String(describing: latitude!)))
+    }
+    
 }
