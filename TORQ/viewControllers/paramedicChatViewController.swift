ea3f8df2b60ee66b083f9e96c39b2f1a4e32c1d2
Nouraaalsulayfih <Nouraaalsulayfih@gmail.com>
2021-12-06 02:40:55 +0300

    connecting IBoutlets and the back button

--- TORQ/viewControllers/paramedicChatViewController.swift
+++ TORQ/viewControllers/paramedicChatViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class paramedicChatViewController: UIViewController {
 
+    
+    //MARK: - @IBOutlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var requesterName: UILabel!
+    @IBOutlet weak var callButton: UIButton!
+    
+    //MARK: - Variables
+    
+    //MARK: - Overriden Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - @IBActions
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
+
+//MARK: - Extension
